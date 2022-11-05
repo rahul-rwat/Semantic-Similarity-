f18970aae655f81397f563001f1313c836494188
rahul-rwat <115935495+rahul-rwat@users.noreply.github.com>
2022-11-06 01:44:22 +0530

    Add files via upload

---
+++ precily_part_a.py
@@ -0,0 +1,19 @@
+from sentence_transformers import SentenceTransformer, util
+import numpy as np
+
+#semantic similarity model 
+model=SentenceTransformer('stsb-roberta-large')
+
+#input 
+sentence1=input("input1:")
+sentence2=input("input2:")
+
+# compute similarity scores of two embeddings
+embedding1=model.encode(sentence1,convert_to_tensor=True)
+embedding2=model.encode(sentence2,convert_to_tensor=True)
+
+cosine_scores=util.pytorch_cos_sim(embedding1, embedding2)
+
+print("text 1:", sentence1)
+print("text 2:", sentence2)
+print("similarity score:", cosine_scores.item())
---
+++ precily_part_b.py
@@ -0,0 +1,24 @@
+from sentence_transformers import SentenceTransformer, util
+import numpy as np
+import streamlit as st
+
+#Header
+st.header('Enter input1 -> Press Enter')
+st.header('Enter input2 -> Press Enter')
+
+#semantic similarity model 
+model=SentenceTransformer('stsb-roberta-large')
+
+#input 
+sentence1=st.text_input("input1:")
+sentence2=st.text_input("input2:")
+
+# compute similarity scores of two embeddings
+embedding1=model.encode(sentence1,convert_to_tensor=True)
+embedding2=model.encode(sentence2,convert_to_tensor=True)
+
+cosine_scores=util.pytorch_cos_sim(embedding1, embedding2)
+
+st.write("text 1:", sentence1)
+st.write("text 2:", sentence2)
+st.write("similarity score:", cosine_scores.item())
