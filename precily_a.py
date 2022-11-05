17a5a20ec0a1b83bfa76ec794f4205825533b2fa
rahul-rwat <115935495+rahul-rwat@users.noreply.github.com>
2022-11-05 22:54:19 +0530

    Update precily_a.py

--- precily_a.py
+++ precily_a.py
@@ -6,33 +6,32 @@ import pandas as pd
 #semantic similarity model 
 model=SentenceTransformer('stsb-roberta-large')
 
-# Open file
-#with open('Precily_Text_Similarity.csv') as file_obj:
 
-    # Skips the heading
-    #heading = next(file_obj)  
-    
-        #reader_obj = csv.DictReader(file_obj)
-
-    # Iterate over each row in the csv file
-    # using reader object
-    #for row in reader_obj:
         
 data1=pd.read_csv('Precily_Text_Similarity.csv',usecols=['text1'])
+df1=(pd.DataFrame(data1))
 data2=pd.read_csv('Precily_Text_Similarity.csv',usecols=['text2'])
-#data.text1
+df2=pd.DataFrame(data2)
+
+df1.set_index("text1", inplace = True)
+df2.set_index("text2", inplace = True)
 
 
 x=range(1,5)
-for x in data1:
-    sentence1=(data1[x])
-    embedding1=model.encode(sentence1,convert_to_tensor=True)
-for x in data2:
-    sentence2=(data2[x])
-    embedding2=model.encode(sentence2,convert_to_tensor=True)
+for x in range(1,5):
+    sentence1=(df1.iloc[:, [0,5]])
+print("text 1:",sentence1)
 
-    # compute similarity scores of two embeddings
-    cosine_scores=util.pytorch_cos_sim(embedding1, embedding2)
+for x in range(1,5):
+    sentence2=(df2.iloc[:, [0,5]])
+print("text 2:",sentence2)
+
+
+# compute similarity scores of two embeddings
+embedding1=model.encode(sentence1,convert_to_tensor=True)
+embedding2=model.encode(sentence2,convert_to_tensor=True)
+
+cosine_scores=util.pytorch_cos_sim(embedding1, embedding2)
 
 print("text 1:", sentence1)
 print("text 2:", sentence2)
