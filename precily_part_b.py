e9c8f4c54909b33ec052f4d193ae45b72d5e8d61
rahul-rwat <115935495+rahul-rwat@users.noreply.github.com>
2022-11-06 15:01:38 +0530

    Update precily_part_b.py

--- precily_part_b.py
+++ precily_part_b.py
@@ -14,8 +14,8 @@ sentence1=st.text_input("input1:")
 sentence2=st.text_input("input2:")
 
 # compute similarity scores of two embeddings
-#embedding1=model.encode(sentence1,convert_to_tensor=True)
-#embedding2=model.encode(sentence2,convert_to_tensor=True)
+embedding1=model.encode(sentence1,convert_to_tensor=True)
+embedding2=model.encode(sentence2,convert_to_tensor=True)
 
 cosine_scores=util.pytorch_cos_sim(embedding1, embedding2)
 
